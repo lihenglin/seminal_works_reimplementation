1565401aeec8dd49c6d2cc753cb6c9e2674280f2
lihenglin <lhlin@stanford.edu>
2024-01-02 00:37:09 -0800

    transformer_pytorch basic ok

--- architectures/transformer_pytorch.py
+++ architectures/transformer_pytorch.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import einops
 import math
+from tqdm import tqdm
 
 
 class MHA(nn.Module):
@@ -22,7 +23,7 @@ class MHA(nn.Module):
 
         # Projection
         Q = self.qproj(x)
-        K, V = einops.rearrange(self.kvproj(y), 'B L (2 d_model) -> B L d_model 2').unbind(dim=-1)
+        K, V = einops.rearrange(self.kvproj(y), 'B L (split d_model) -> B L d_model split', split=2).unbind(dim=-1)
 
         # Rearrange to multi-head Q, K, V
         Q = einops.rearrange(Q, 'B L (num_heads d_k) -> B num_heads L d_k', num_heads=self.num_heads)
@@ -57,7 +58,7 @@ class TransformerEncoderBlock(nn.Module):
 
     def forward(self, x, mask=None):
         # Pre-Norm architecture is less sensitive to learning rate warmup
-        x = x + F.dropout(self.mha(self.mha_norm(x), mask), p=self.dropout_rate)
+        x = x + F.dropout(self.mha(self.mha_norm(x), mask=mask), p=self.dropout_rate)
         x = x + F.dropout(self.ffn(self.ffn_norm(x)), p=self.dropout_rate) 
         return x
 
@@ -86,15 +87,17 @@ class TransformerEncoder(nn.Module):
 
         self.emb_lookup = nn.Embedding(num_embeddings=256, embedding_dim=d_model) # Just a placeholder. Can have different ways to get the embeddings for different applications
         self.pos_emb_lookup = PositionalEncoding(d_model, max_seq_len)
-        self.transformer = nn.Sequential(
-            *[TransformerEncoderBlock(d_model, num_heads, dropout_rate) for _ in range(6)]
+        self.transformer = nn.ModuleList(
+            [TransformerEncoderBlock(d_model, num_heads, dropout_rate) for _ in range(6)]
         )
         self.norm = nn.LayerNorm(d_model)
         self.classifier = nn.Linear(d_model, num_classes)
 
     def forward(self, x, mask=None):
         emb = F.dropout(self.emb_lookup(x) * self.emb_scale + self.pos_emb_lookup(x), p=self.dropout_rate) # NOTE: I can't find a reasonable explanation for self.emb_scale, but seems to be what people usually do
-        return self.classifier(self.norm(self.transformer(emb, mask)))
+        for layer in self.transformer:
+            emb = layer(emb, mask=mask)
+        return self.classifier(self.norm(emb))
 
 
 class TransformerDecoderBlock(nn.Module):
@@ -122,11 +125,11 @@ class LM(nn.Module):
     def __init__(self, num_classes=10, max_seq_len=1024):
         super(LM, self).__init__()
         self.transformer = TransformerEncoder(num_classes=num_classes, max_seq_len=max_seq_len)
-        self.mask = torch.triu(torch.full((max_seq_len, max_seq_len), float('-inf')), diagonal=1)
+        self.register_buffer('mask', torch.triu(torch.full((max_seq_len, max_seq_len), float('-inf')), diagonal=1))
 
     def forward(self, x):
-        # TODO: append a null token at the beginning of the sequence
-        pass
+        x = torch.cat((255 * torch.ones_like(x[:, 0:1]), x[:, :-1]), dim=1) # NOTE: 0xFF never occur in enwik9
+        return self.transformer(x, mask=self.mask)
 
 
 ### Just for testing purpose ###
@@ -162,16 +165,25 @@ class Enwik9Loader:
         return iter(batches)
 
 if __name__ == '__main__':
-    # TODO: [Test] use enwik9 and monitor loss
-    dataloader = list(Enwik9Loader(batch_size=100, seq_len=256, datapath='data/enwik9'))
+    dataloader = list(Enwik9Loader(batch_size=100, seq_len=256, datapath='/iliad/u/lhlin/seminal_works_reimplementation/datasets/enwik9'))
     num_classes, max_seq_len = 256, 256
 
-    LM = LM(num_classes, max_seq_len)
-    optimizer = torch.optim.Adam()
+    LM = LM(num_classes, max_seq_len).cuda()
+    optimizer = torch.optim.Adam(LM.parameters(), lr=3e-4)
+    # TODO: linear learning rate decay
+    
+    loss_fn = nn.CrossEntropyLoss()
 
-    for i, batch in enumerate(dataloader):
-        # TODO: what does the data look like? words or dicrete tokens?
-        print(batch)
-        pass
+    avg_loss = 0.
+    for i, batch in tqdm(enumerate(dataloader)):
+        batch = torch.tensor(batch, dtype=torch.long).cuda()
+        loss = loss_fn(einops.rearrange(LM(batch), 'B L C -> B C L'), batch)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        avg_loss += loss.item()
+        if i % 100 == 0:
+            print(f"Loss: {avg_loss / 100}")
+            avg_loss = 0.
 
     # TODO: low precision training
